95c45e0de096464d47982388d94bfebf39ca57e2
mustafa-raza-26 <mustafa778823@gmail.com>
2026-04-26 00:01:27 +0500

    Feane Web

---
+++ app.js
@@ -0,0 +1,82 @@
+// let display = document.getElementById('display');
+// async function fetchData() {
+
+//     const API = await fetch('https://dummyjson.com/recipes');
+//     const data = await API.json();
+//     console.log(data.recipes);    
+
+//     for (let i = 0; i < data.recipes.length; i++) {
+//         let recipes = data.recipes[i];
+//         console.log(recipes);
+
+//         display.innerHTML += `
+//             <div class="crd p-0 col-11 col-md-3">
+//             <div class="image"><img src="${recipes.image}" alt=""></div>
+//             <div class="cd-content curve">
+//               <h5>${recipes.name}</h5>
+//               <p>Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque</p>
+//             <div class="d-flex justify-content-between pt-2">
+//               <div>$20</div>
+//               <div><i class="fa-solid fa-cart-shopping cart"></i></div>
+//             </div>
+//           </div>
+//           </div>
+//         `
+//     }
+
+
+// }
+// fetchData();
+
+let display = document.getElementById('display');
+let viewMoreBtn = document.getElementById('view_Btn');
+
+let allRecipes = [];
+let showCount = 9;
+
+async function fetchData() {
+    const API = await fetch('https://dummyjson.com/recipes');
+    const data = await API.json();
+
+    allRecipes = data.recipes; // sab data store kar liya
+    renderData();
+}
+
+function renderData() {
+    display.innerHTML = "";
+
+    let visibleData = allRecipes.slice(0, showCount);
+
+    visibleData.forEach(recipes => {
+        display.innerHTML += `
+            <div class="crd p-0 col-11 col-md-3">
+              <div class="image">
+                <img src="${recipes.image}" alt="">
+              </div>
+              <div class="cd-content curve">
+                <h5>${recipes.name}</h5>
+                <p>Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque</p>
+                <div class="d-flex justify-content-between pt-2">
+                  <div>$20</div>
+                  <div><i class="fa-solid fa-cart-shopping cart"></i></div>
+                </div>
+              </div>
+            </div>
+        `;
+    });
+
+    // button hide/show
+    if (showCount >= allRecipes.length) {
+        viewMoreBtn.style.display = "none";
+    } else {
+        viewMoreBtn.style.display = "block";
+    }
+}
+
+// View More click
+viewMoreBtn.addEventListener("click", () => {
+    showCount = allRecipes.length; // sab show
+    renderData();
+});
+
+fetchData();
